7e5fac3d2a7dedeff549575fe30f63d581c83f82
Ryan2144 <70672438+Ryan2144@users.noreply.github.com>
2024-09-20 20:57:01 -0500

    2147483648 Remastered

---
+++ js/skin_chemistry.js
@@ -0,0 +1,85 @@
+window.requestAnimationFrame(function () {
+  var game = new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager);
+  game.actuator.addTile = function (tile) {
+    var self = this;
+    var wrapper   = document.createElement("div");
+    var inner     = document.createElement("div");
+    var position  = tile.previousPosition || { x: tile.x, y: tile.y };
+    positionClass = this.positionClass(position);
+    var classes = ["tile", "tile-" + tile.value, positionClass];
+    if (tile.value > 65536) classes.push("tile-super");
+    this.applyClasses(wrapper, classes);
+    inner.classList.add("tile-inner");
+    switch (tile.value) {
+    case 2:
+      inner.textContent = "H";
+      break;
+    case 4:
+      inner.textContent = "He";
+      break;
+    case 8:
+      inner.textContent = "Li";
+      break;
+    case 16:
+      inner.textContent = "Be";
+      break;
+    case 32:
+      inner.textContent = "B";
+      break;
+    case 64:
+      inner.textContent = "C";
+      break;
+    case 128:
+      inner.textContent = "N";
+      break;
+    case 256:
+      inner.textContent = "O";
+      break;
+    case 512:
+      inner.textContent = "F";
+      break;
+    case 1024:
+      inner.textContent = "Ne";
+      break;
+    case 2048:
+      inner.textContent = "Na";
+      break;
+    case 4096:
+      inner.textContent = "Mg";
+      break;
+    case 8192:
+      inner.textContent = "Al";
+      break;
+    case 16384:
+      inner.textContent = "Si";
+      break;
+    case 32768:
+      inner.textContent = "P";
+      break;
+    case 65536:
+      inner.textContent = "S";
+      break;
+    case 131072:
+      inner.textContent = "Cl";
+      break;
+    }
+    if (tile.previousPosition) {
+      window.requestAnimationFrame(function () {
+        classes[2] = self.positionClass({ x: tile.x, y: tile.y });
+        self.applyClasses(wrapper, classes);
+      });
+    } else if (tile.mergedFrom) {
+      classes.push("tile-merged");
+      this.applyClasses(wrapper, classes);
+      tile.mergedFrom.forEach(function (merged) {
+        self.addTile(merged);
+      });
+    } else {
+      classes.push("tile-new");
+      this.applyClasses(wrapper, classes);
+    }
+    wrapper.appendChild(inner);
+    this.tileContainer.appendChild(wrapper);
+    };
+    game.restart();
+});
