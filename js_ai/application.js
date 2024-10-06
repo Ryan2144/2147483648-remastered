a615612a9a9bee8c7c785d0d69ca68bb89f90481
Ryan2144 <70672438+Ryan2144@users.noreply.github.com>
2024-10-06 18:23:34 -0500

    2147483648 AI

---
+++ js_ai/application.js
@@ -0,0 +1,6 @@
+animationDelay = 1;
+
+// Wait till the browser is ready to render the game (avoids glitches)
+window.requestAnimationFrame(function () {
+  new GameManager(8, KeyboardInputManager, HTMLActuator, LocalStorageManager);
+});
